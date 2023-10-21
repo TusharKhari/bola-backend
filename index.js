014e6f4b231a205f483d31669065878ffd04a61c
TusharKhari <tusharkhari1@gmail.com>
2023-10-21 17:52:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+console.log("helooo");
+
+const express = require('express');
+const app = express();
+
+//dotenv
+require('dotenv').config();
+
+//mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://ubola:ps123456@ubolacluster.pa4487l.mongodb.net/ubola');
+// mongoose.connect('mongodb+srv://ubola:ps123456@ubolacluster.tfw22hc.mongodb.net/ubola');
+
+mongoose.connection.once('open',()=>{
+    console.log('mongoose connected');
+});
+
+mongoose.connection.on('error',()=>{
+    console.log('Mongoose connection failed '+error);
+});
+
+// //user route
+const userRoute = require('./route/UserRoute');
+app.use('/api',userRoute);
+
+app.use("/", (req , res)=>{ 
+    res.send("Home Page");
+});
+
+// //notification route
+// const notifyRoute = require('./route/NotificationRoute');
+// app.use('/api',notifyRoute);
+
+// //server
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on ${process.env.PORT}`);
+});
